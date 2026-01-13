9bb3c09e0e0fd92475cbf2c584ceaf257d8f839e
TheMalevolentOne1 <kylerobinson818@gmail.com>
2026-01-13 17:23:53 +0000

    Changed all html to one shared navbar

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -40,6 +40,49 @@ const handleLogout = async () =>
 }
 
 /*
+Brief: Delete Account API Call
+*/
+const delAccount = async () => 
+{
+    const email = prompt("Please enter your email to confirm account deletion:");
+ 
+    if (!email) 
+    {
+        alert('Email is required to delete account.');
+        return;
+    }
+    else
+    {
+        try {
+            const res = await fetch('/auth/whoami', {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+            });
+            
+            const data = await res.json();
+
+            if (data.email !== email) {
+                alert('The email you entered does not match your account email.');
+                return;
+            } else {
+                const response = await fetch('/user/del-acc/', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ uuid: data.userId }),
+                });
+                if (response.ok) {
+                    alert('Account deleted successfully.');
+                    window.location.href = "../landing_page.html"; // Redirect to a goodbye page or homepage
+                }
+            }
+        } catch (error) {
+            console.error('Error deleting account:', error);
+            alert('An error occurred.');
+        }
+    }
+}
+
+/*
 Brief: dashboard.js - Dashboard Page Functionality
 */
 document.addEventListener('DOMContentLoaded', async () => {
